d9b9e18a78ad284a89bc1df0ef126246a493a6c2
Bryan <bryanscottarnold@gmail.com>
2025-07-22 04:39:40 -0400

    add init py files to bugs

--- laCucaracha/bugs/__init__.py
+++ laCucaracha/bugs/__init__.py
@@ -4,4 +4,4 @@ from .format_bug import FormatBug
 from .logic_bug import LogicBug
 from .syntax_bug import SyntaxBug
 
-__all__ = ["TypoBug"]
+__all__ = ["TypoBug", "ImportBug", "FormatBug", "LogicBug", "SyntaxBug"]
