5ccfc2e4f92b28423138e138cfe91d08c714665a
Bryan <bryanscottarnold@gmail.com>
2025-08-27 01:49:51 -0400

    bugsprai format_fixer, improvements on block scope, but still needs refining

--- bugSprAI/fixers/format_fixer.py
+++ bugSprAI/fixers/format_fixer.py
@@ -17,7 +17,7 @@ class FormatFixer(BaseFixer):
 
     def fallback_format(self, code: str) -> str:
         fixed_lines = []
-        scope_stack = []
+        scope_stack = []  # (scope_type, opener_indent, body_indent)
 
         lines = code.splitlines()
 
@@ -28,38 +28,56 @@ class FormatFixer(BaseFixer):
                 continue
 
             leading_spaces = len(line) - len(stripped)
-
-            while scope_stack and leading_spaces < scope_stack[-1][1]:
-                scope_stack.pop()
-
             block_openers = ('def', 'class', 'if', 'for', 'while', 'try', 'with')
             block_followups = ('elif', 'else', 'except', 'finally')
 
             first_word = stripped.split()[0]
 
+            # Pop scopes if current line is before opener_indent
+            while scope_stack and leading_spaces < scope_stack[-1][1]:
+                scope_stack.pop()
+
+            # Handle followups (except/else/elif/finally)
             if first_word in block_followups:
-                if scope_stack:
-                    indent = scope_stack[-1][1]
+                # Find nearest matching opener (try/if/for/etc.)
+                for i in range(len(scope_stack)-1, -1, -1):
+                    if scope_stack[i][0] in ('if', 'for', 'while', 'try', 'with'):
+                        opener_indent = scope_stack[i][1]
+                        break
                 else:
-                    indent = 0
+                    opener_indent = 0
+
                 if not stripped.endswith(':'):
                     stripped += ':'
-                scope_stack.append((first_word, indent, indent + 4))
-                fixed_lines.append(' ' * indent + stripped)
+
+                scope_stack.append((first_word, opener_indent, opener_indent + 4))
+                fixed_lines.append(' ' * opener_indent + stripped)
                 continue
 
+            # Handle block openers (def/class/if/etc.)
             if first_word in block_openers:
-                indent = leading_spaces
                 if not stripped.endswith(':'):
                     stripped += ':'
-                scope_stack.append((first_word, indent, indent + 4))
-                fixed_lines.append(' ' * indent + stripped)
+
+                # Determine opener indent
+                if first_word in ('def', 'class'):
+                    # top-level def/class always 0 indent
+                    opener_indent = 0
+                else:
+                    # other blocks align with nearest parent or use current leading_spaces
+                    parent_body_indent = scope_stack[-1][2] if scope_stack else 0
+                    opener_indent = max(leading_spaces, parent_body_indent)
+
+                body_indent = opener_indent + 4
+                scope_stack.append((first_word, opener_indent, body_indent))
+                fixed_lines.append(' ' * opener_indent + stripped)
                 continue
 
+            # Normal line
             if scope_stack:
-                indent = scope_stack[-1][2]
+                indent = max(leading_spaces, scope_stack[-1][2])
             else:
-                indent = 0
+                indent = leading_spaces
             fixed_lines.append(' ' * indent + stripped)
 
         return "\n".join(fixed_lines)
